3a66f23ce55aa38bd32611507e0d1dc96ce9596c
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-03-26 22:26:10 +0300

    completed Cards component

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import cardsData from "../api/cards";
 
 const Cards = () => {
-  return (
-    <div>Cards</div>
-  )
-}
+  const [cards, setCards] = useState([]);
 
-export default Cards
+  useEffect(() => {
+    //data isteği
+    setCards(cardsData);
+  }, []);
+
+  return (
+    <div className="grid grid-cols-3 gap-x-4">
+      {cards.length &&
+        cards.map((card) => (
+          <div className="bg-white p-10 rounded-lg shadow-sm flex flex-col items-center text-center">
+            <img className="w-[150px] h-[150px] mb-6" src={card.image} />
+            <h6 className="font-semibold text-lg text-primary-brand-color">{card.title}</h6>
+            <p className="mt-2 text-sm text-gray-700">{card.description}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Cards;
