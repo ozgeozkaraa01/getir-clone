32647488d76e50ea0eb4ecef536e1cd6609a6b7c
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-03-27 12:23:57 +0300

    responsive Categories component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import Menu from "./ui/Menu";
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
-import { BiGlobe } from 'react-icons/bi'
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { BiGlobe } from "react-icons/bi";
 
 const Footer = () => {
   const menus = [
@@ -78,13 +78,22 @@ const Footer = () => {
               <h6 className="text-lg text-primary-brand-color">
                 Getir'i indirin!
               </h6>
-              <Link href="#">
+              <Link
+                href="https://apps.apple.com/app/id995280265"
+                target="_blank"
+              >
                 <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" />
               </Link>
-              <Link href="#">
+              <Link
+                href="https://play.google.com/store/apps/details?id=com.getir"
+                target="_blank"
+              >
                 <img src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg" />
               </Link>
-              <Link href="#">
+              <Link
+                href="https://appgallery.huawei.com/#/app/C100954039"
+                target="_blank"
+              >
                 <img src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg" />
               </Link>
             </nav>
@@ -97,7 +106,8 @@ const Footer = () => {
           <div className="text-xs text-gray-700 flex gap-x-8">
             &copy; 2023 Getir
             <Link
-              href="#"
+              href="https://e-sirket.mkk.com.tr/?page=company&company=18806"
+              target="_blank"
               className="text-primary-brand-color hover:underline relative before:w-[3px] before:h-[3px] before:bg-black before:absolute before:-left-4 before:top-1/2 before:-translate-y-1/2 before:rounded-full"
             >
               Bilgi Toplumu Hizmetleri
@@ -105,19 +115,22 @@ const Footer = () => {
           </div>
           <nav className="flex gap-x-3">
             <Link
-              href="#"
+              href="https://www.facebook.com/getir"
+              target="_blank"
               className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center"
             >
               <FaFacebook size={21} />
             </Link>
             <Link
-              href="#"
+              href="https://twitter.com/getir"
+              target="_blank"
               className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center"
             >
               <FaTwitter size={21} />
             </Link>
             <Link
-              href="#"
+              href="https://www.instagram.com/getir/"
+              target="_blank"
               className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center"
             >
               <FaInstagram size={21} />
